9b6982a64a383f73792baa63c85af1e65146db74
zzqmatebook <475411577@qq.com>
2024-03-16 11:41:22 +0800

    本地模型调试页面定型

--- GUICode.py
+++ GUICode.py
@@ -2,52 +2,6 @@ import streamlit as st
 from LocalFile import LocalPromptYml
 
 class GLMui():
-    def __init__(self):
-        self.prompt = "这里是聊天内容"
-    def loaclGLMPage(self):
-        with st.container():
-            #标题页面的容器
-            st.header("JJCK_GLM调试页面",divider="blue")
-            # st.title("本地GLM调试页面")
-
-        with st.container():
-            # 侧边栏的容器
-            funclist=st.sidebar.container(height=520,border=False)
-            # st.sidebar.title("功能列表")
-            # st.session_state["chatlist"]=st.sidebar.selectbox("用户对话",["对话1","对话2"])
-            # opt=st.sidebar.selectbox("提示词模板选择",["袁祖成","梁桂和","基础模板"])
-            # print(opt)
-            funclist.title("GLM功能列表")
-            st.session_state["chatlist"] = funclist.selectbox("用户对话", ["袁祖成", "梁桂和"])
-            opt = funclist.selectbox("提示词模板选择", ["袁祖成", "梁桂和", "基础模板"])
-            print(opt)
-            buttonlist=st.sidebar.container(height=100,border=False)
-            if buttonlist.button("清空历史会话"):
-                st.session_state["chat1history"] = []
-
-
-        with st.container():
-            # 聊天框内容
-            messages = st.container(height=450,border=False)
-            if st.session_state.chatlist=="袁祖成":
-            # 初始化输入框和输出框
-            #     messages.chat_message(name="user", avatar="user")
-                    # input_placeholder = st.empty()
-                # with st.chat_message(name="assistant", avatar="assistant"):
-                #     message_placeholder = st.empty()
-                if "chat1history" not in st.session_state:
-                    st.session_state["chat1history"] = []  # 把用户输入的prompt存入session_state，形成历史记录
-                if prompt := st.chat_input("用户输入问题，按回车发送，shinf+回车换行"):
-                    st.session_state["chat1history"].append({"User": {prompt}})
-                    # input_placeholder.markdown(f"User: {prompt}")
-                    # print(messages)
-                    print(st.session_state["chat1history"])
-                    for user_message in st.session_state["chat1history"]:
-                        # messages.markdown(f"User: {user_message['User']}")
-                        messages.chat_message("uesr").write(user_message['User'])
-                        print(user_message['User'])
-            else:
-                st.markdown("这是另外一个页面了")
     def chatGLMPahe(self):
         chatpage,promppage=st.tabs(["GLM对话","提示词配置"]) #对页面进行标签分页
         if "promtnamelist" not in st.session_state:
@@ -86,15 +40,27 @@ class GLMui():
                 #根据标签名称读取不同人员的模板内容，人员列表是固定的。
                 if selectresult ==promtname:
                     if promtname=="袁祖成模板":
-                        prompt_name = prompt_msgcontainer.text_input(f"{selectresult}内容", value=_ymlcontent.get("yzc"))
+                        prompt_content = prompt_msgcontainer.text_area(f"{selectresult}内容", value=_ymlcontent.get("yzc"),height=300)
+                        _ymlcontent["yzc"]=prompt_content #赋值到写入本地文档的方法
+                        # prompt_input=prompt_content #获取更新后的Yml文件内容
                     elif promtname=="梁桂和模板":
-                        prompt_name = prompt_msgcontainer.text_input(f"{selectresult}内容", value=_ymlcontent.get("lgh"))
+                        prompt_content = prompt_msgcontainer.text_area(f"{selectresult}内容", value=_ymlcontent.get("lgh"))
+                        _ymlcontent["lgh"]=prompt_content
+                        # prompt_input=prompt_content
                     elif promtname=="基础模板":
-                        prompt_name = prompt_msgcontainer.text_input(f"{selectresult}内容", value=_ymlcontent.get("base"))
-                    print(prompt_name)
+                        prompt_content = prompt_msgcontainer.text_area(f"{selectresult}内容", value=_ymlcontent.get("base"))
+                        _ymlcontent["base"]=prompt_content
+                        # prompt_input=prompt_content
+                    # print(_ymlcontent)
             but1,but2,but3=st.columns([1,1,5]) #实现两个按钮能整齐排列
-            but1.button("修改模板",on_click=self._writepromptcontent,args=(prompt_name,))
-            but2.button("取消修改")
+            if 'clicked' not in st.session_state:
+                st.session_state.clicked = False
+            but1.button("修改模板",on_click=self._writepromptcontent,args=(_ymlcontent,))
+            if st.session_state.clicked:
+                # The message and nested widget will remain on the page
+                st.write('Button clicked!')
+                # st.slider('Select a value')
+            but2.button("取消修改") #其实这个没啥用
 
 
 
@@ -120,6 +86,14 @@ class GLMui():
     def _readpromptcontent(self):
         localyml = LocalPromptYml(user_input_path="prompt.yaml")
         return localyml.read_prompt()
-    def _writepromptcontent(self,promptcontent):
-        print(f"这是用户输入{promptcontent}")
+    def _writepromptcontent(self,promptcontent:dict):
+        """
+        将用户输入的更改内容写入yaml写入prompt.yaml文件
+        :param promptcontent: 用户传入的yml内容,必须是{'yzc': 'test', 'base': 'test1', 'lgh': '123456'} 这个类型的才可以
+        :return:
+        """
 
+        st.session_state.clicked = True
+        print(f"这是用户输入{promptcontent}")
+        localyml = LocalPromptYml(user_input_path="prompt.yaml")
+        localyml.write_prompt(promptcontent)
--- LocalFile.py
+++ LocalFile.py
@@ -12,11 +12,22 @@ class LocalPromptYml:
         读取本地yml文件
         :return: 返回读取内容的数组
         """
-        with open(self.file_path, 'r') as file:
+        with open(self.file_path, 'r',encoding='utf-8') as file:
             if self.file_path.endswith('.yaml') or self.file_path.endswith('.yml'):
                 prompt = yaml.safe_load(file)
-                prompt_content=prompt.get("Prompt",{})
+                # prompt_content=prompt.get("Prompt",{})
             else:
                 raise ValueError("Unsupported file format. Supported formats are .json, .yaml, and .yml.")
-            return prompt_content
+            return prompt
 
+    def write_prompt(self,prompt_content):
+        """
+        写入yml文件
+        :param prompt_content: 写入的内容
+        :return:
+        """
+        with open(self.file_path, 'w',encoding='utf-8') as file:
+            if self.file_path.endswith('.yaml') or self.file_path.endswith('.yml'):
+                yaml.safe_dump(prompt_content, file,default_flow_style=False, allow_unicode=True)
+            else:
+                raise ValueError("Unsupported file format. Supported formats are .json, .yaml, and .yml.")
